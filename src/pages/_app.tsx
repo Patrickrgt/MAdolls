d9f85625ce24d73a76ba17b8ca1dc266a28801de
John Doe <patrickrgt@gmail.com>
2024-07-24 22:22:43 -0400

    metadata change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Barlow } from "next/font/google";
+import Head from "next/head";
 
 const barlow = Barlow({
   weight: ["200", "300", "400", "500", "600", "700"],
@@ -9,8 +10,17 @@ const barlow = Barlow({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={barlow.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta property="og:title" content="MAdoll" />
+        <meta property="og:description" content="This is a generative collection in which MAcci, as an individual creator, challenges NFT's and continues to search of possibilities." />
+        <meta property="og:image" content="https://i.imgur.com/VyrNETf.png" />
+        <meta property="og:url" content="https://www.madoll.art/" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <main className={barlow.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
